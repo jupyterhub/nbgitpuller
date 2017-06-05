0c055ceb2c603a4e1a89e43d6600446e9cde9fd4
SaladRaider <peterkangveerman@gmail.com>
2017-06-05 13:25:28 -0700

    Add more tests

--- tests/pull_from_remote_test.py
+++ tests/pull_from_remote_test.py
@@ -1,9 +1,9 @@
 import os
 import shutil
+import subprocess
 import random
 import string
 import unittest
-import subprocess
 from gitautosync import pull_from_remote
 
 
@@ -11,23 +11,67 @@ class TestPullFromRemote(unittest.TestCase):
 
     init_path = './summer'
     repo_url = 'https://github.com/data-8/summer'
+    branch_name = 'gh-pages'
 
     def setUp(self):
+        pass
+
+    def test_initialize_repo(self):
         if os.path.exists(self.init_path):
             shutil.rmtree(self.init_path)
-
-        subprocess.run(['gitautosync'])
+        pull_from_remote._initialize_repo(self.repo_url, self.init_path)
+        self.assertTrue(os.path.exists(self.init_path))
+        self.assertTrue(os.path.exists(self.init_path + "/.git"))
 
     def test_repo_is_dirty(self):
         result = pull_from_remote._repo_is_dirty(self.init_path)
         self.assertFalse(result)
 
-        new_file_name = "{}/index.html".format(self.init_path)
-        with open(new_file_name, 'w') as file:
-            file.write(self.generate_random_string(10))
+        self.make_repo_dirty()
 
         result = pull_from_remote._repo_is_dirty(self.init_path)
         self.assertTrue(result)
+
+    def test_get_sub_cwd(self):
+        result = pull_from_remote._get_sub_cwd(self.init_path)
+        self.assertTrue(os.path.exists(result))
+
+    def test_make_commit(self):
+        self.make_repo_dirty()
+        pull_from_remote._make_commit(self.init_path, self.branch_name)
+        cwd = pull_from_remote._get_sub_cwd(self.init_path)
+        ps = subprocess.Popen(['git', 'log', '--oneline'], stdout=subprocess.PIPE, cwd=cwd)
+        output = subprocess.check_output(['head', '-n', '1'], stdin=ps.stdout, cwd=cwd)
+        ps.wait()
+        output = output.decode('utf-8').strip().split(' ')[1]
+        self.assertEqual(output, 'WIP')
+
+        result = pull_from_remote._repo_is_dirty(self.init_path)
+        self.assertFalse(result)
+
+    def test_pull_and_resolve_conflicts(self):
+        cwd = pull_from_remote._get_sub_cwd(self.init_path)
+
+        with open(self.init_path + "/new_file1.txt", 'w') as new_file:
+            new_file.write(self.generate_random_string(10))
+
+        pull_from_remote._make_commit(self.init_path, self.branch_name)
+
+        with open(self.init_path + "/new_file2.txt", 'w') as new_file:
+            new_file.write(self.generate_random_string(10))
+
+        pull_from_remote._pull_and_resolve_conflicts(self.repo_url, self.init_path, self.branch_name)
+
+        self.assertTrue(os.path.exists(self.init_path + "/new_file2.txt"))
+        self.assertTrue(os.path.exists(self.init_path + "/new_file1.txt"))
+
+        result = subprocess.check_output(['git', 'status'], cwd=cwd).decode('utf-8')
+        self.assertIn('ahead', result)
+
+    def make_repo_dirty(self):
+        new_file_name = "{}/index.html".format(self.init_path)
+        with open(new_file_name, 'w') as file:
+            file.write(self.generate_random_string(10))
 
     def generate_random_string(self, N):
         return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(N))
