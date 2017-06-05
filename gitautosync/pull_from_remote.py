2a90b94ef16fde282fab686b7e5746e1294f2fdb
SaladRaider <peterkangveerman@gmail.com>
2017-06-05 11:19:54 -0700

    Refactor

--- gitautosync/pull_from_remote.py
+++ gitautosync/pull_from_remote.py
@@ -18,8 +18,7 @@ def pull_from_remote(git_url, branch_name, repo_dir):
     if not os.path.exists(repo_dir):
         _initialize_repo(git_url, repo_dir)
     else:
-        _make_commit_if_dirty(repo_dir, branch_name)
-        _pull_and_resolve_conflicts(git_url, repo_dir, branch_name)
+        _update_repo(git_url, repo_dir, branch_name)
 
     logger.info('Pulled from repo: {}'.format(git_url))
 
@@ -28,33 +27,45 @@ def _initialize_repo(git_url, repo_dir):
     """
     Clones repository.
     """
+    assert git_url
+
     logger.info('Repo {} doesn\'t exist. Cloning...'.format(repo_dir))
-
-    # Clone repo
     subprocess.check_call(['git', 'clone', git_url, repo_dir])
-
     logger.info('Repo {} initialized'.format(repo_dir))
 
 
-def _make_commit_if_dirty(repo_dir, branch_name):
+def _update_repo(git_url, repo_dir, branch_name):
     """
-    Makes a commit with message 'WIP' if there are changes.
+    Update repo by merging local and upstream changes
     """
-    cwd = _get_sub_cwd(repo_dir)
+    assert git_url and branch_name
+
     if _repo_is_dirty(repo_dir):
-        subprocess.check_call(['git', 'checkout', branch_name], cwd=cwd)
-        subprocess.check_call(['git', 'add', '-A'], cwd=cwd)
-        subprocess.check_call(['git', 'commit', '-m', 'WIP'], cwd=cwd)
-        logger.info('Made WIP commit')
+        _make_commit(repo_dir, branch_name)
+    _pull_and_resolve_conflicts(git_url, repo_dir, branch_name)
+
+
+def _make_commit(repo_dir, branch_name):
+    """
+    Commit local changes
+    """
+    assert branch_name
+
+    cwd = _get_sub_cwd(repo_dir)
+    subprocess.check_call(['git', 'checkout', branch_name], cwd=cwd)
+    subprocess.check_call(['git', 'add', '-A'], cwd=cwd)
+    subprocess.check_call(['git', 'commit', '-m', 'WIP'], cwd=cwd)
+    logger.info('Made WIP commit')
 
 
 def _pull_and_resolve_conflicts(git_url, repo_dir, branch_name):
     """
     Git pulls, resolving conflicts with -Xours
     """
+    assert git_url and branch_name
+
     logger.info('Starting pull from {}'.format(git_url))
 
-    # Fetch then merge, resolving conflicts by keeping original content
     cwd = _get_sub_cwd(repo_dir)
     subprocess.check_call(['git', 'checkout', branch_name], cwd=cwd)
     subprocess.check_call(['git', 'fetch'], cwd=cwd)
@@ -64,10 +75,17 @@ def _pull_and_resolve_conflicts(git_url, repo_dir, branch_name):
 
 
 def _repo_is_dirty(repo_dir):
+    """
+    Return empty string if repo not dirty.
+    Return non-empty string if repo dirty.
+    """
     cwd = _get_sub_cwd(repo_dir)
     out = subprocess.check_output(['git', 'diff-index', '--name-only', 'HEAD', '--'], cwd=cwd)
     return out
 
 
 def _get_sub_cwd(repo_dir):
+    """
+    Get sub dir name from current workind directory
+    """
     return '{}/{}'.format(os.getcwd(), repo_dir)
