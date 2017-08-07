b3a1a1f44fb3f3246ce0779b6f575f5b47518d89
yuvipanda <yuvipanda@gmail.com>
2017-08-07 14:00:45 -0700

    Move gitautosync to __init__.py
    Single file!

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     install_requires=dependencies,
     entry_points={
         'console_scripts': [
-            'gitautosync = gitautosync.gitautosync:main',
+            'gitautosync = gitautosync:main',
         ],
     },
     classifiers=[
