8ffc33e828f480712ead9570b1e89c5212186670
yuvipanda <yuvipanda@gmail.com>
2017-08-08 00:01:40 -0700

    Use a lock to make sure there's only one git operation at a time

--- nbgitautosync/handlers.py
+++ nbgitautosync/handlers.py
@@ -1,4 +1,5 @@
-from tornado import gen, web
+from tornado import gen, web, locks
+
 from gitautosync import GitAutoSync
 from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler
@@ -10,6 +11,18 @@ import jinja2
 
 
 class SyncHandler(IPythonHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # We use this lock to make sure that only one sync operation
+        # can be happening at a time. Git doesn't like concurrent use!
+        if 'git_lock' not in self.settings:
+            self.settings['git_lock'] = locks.Lock()
+
+    @property
+    def git_lock(self):
+        return self.settings['git_lock']
+
     @gen.coroutine
     def emit(self, data):
         if type(data) is not str:
@@ -21,38 +34,45 @@ class SyncHandler(IPythonHandler):
 
     @gen.coroutine
     def get(self):
-        repo = self.get_argument('repo')
-        branch = self.get_argument('branch')
-        repo_dir = repo.split('/')[-1]
+        try:
+            yield self.git_lock.acquire(1)
+        except gen.TimeoutError:
+            raise web.HTTPError(429, 'Another sync operation in progress, try again in a few minutes')
+        try:
+            repo = self.get_argument('repo')
+            branch = self.get_argument('branch')
+            repo_dir = repo.split('/')[-1]
 
-        # We gonna send out event streams!
-        self.set_header('content-type', 'text/event-stream')
-        self.set_header('cache-control', 'no-cache')
+            # We gonna send out event streams!
+            self.set_header('content-type', 'text/event-stream')
+            self.set_header('cache-control', 'no-cache')
 
-        gas = GitAutoSync(repo, branch, repo_dir)
+            gas = GitAutoSync(repo, branch, repo_dir)
 
-        q = Queue()
-        def pull():
-            for line in gas.pull_from_remote():
-                q.put_nowait(line)
-            # Sentinel when we're done
-            q.put_nowait(None)
-        gas_thread = threading.Thread(target=pull)
+            q = Queue()
+            def pull():
+                for line in gas.pull_from_remote():
+                    q.put_nowait(line)
+                # Sentinel when we're done
+                q.put_nowait(None)
+            self.gas_thread = threading.Thread(target=pull)
 
-        gas_thread.start()
+            self.gas_thread.start()
 
-        while True:
-            try:
-                progress = q.get_nowait()
-            except Empty:
-                yield gen.sleep(0.5)
-                continue
-            if progress is None:
-                break
+            while True:
+                try:
+                    progress = q.get_nowait()
+                except Empty:
+                    yield gen.sleep(0.5)
+                    continue
+                if progress is None:
+                    break
 
-            self.emit({'output': progress, 'phase': 'Syncing'})
+                self.emit({'output': progress, 'phase': 'syncing'})
 
-        self.emit({'phase': 'Finished'})
+            self.emit({'phase': 'finished'})
+        finally:
+            self.git_lock.release()
 
 class UIHandler(IPythonHandler):
     def initialize(self):
