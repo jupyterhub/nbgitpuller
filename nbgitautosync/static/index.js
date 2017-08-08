dec3357dcfce85a6aa6c663b8002e1a07e9abf8f
yuvipanda <yuvipanda@gmail.com>
2017-08-07 21:45:41 -0700

    More UX tweaks
    - Use xterm from notebook!
    - Vendor fit.js!
    - No more flashing stuff!

--- nbgitautosync/static/index.js
+++ nbgitautosync/static/index.js
@@ -1,7 +1,44 @@
+function fitTerm (term) {
+    // Vendored in from the xterm.js fit addon
+    // Because I can't for the life of me get the addon to be
+    // actually included here as an require.js thing.
+    // And life is too short.
+    if (!term.element.parentElement) {
+        return null;
+    }
+    var parentElementStyle = window.getComputedStyle(term.element.parentElement),
+        parentElementHeight = parseInt(parentElementStyle.getPropertyValue('height')),
+        parentElementWidth = Math.max(0, parseInt(parentElementStyle.getPropertyValue('width')) - 17),
+        elementStyle = window.getComputedStyle(term.element),
+        elementPaddingVer = parseInt(elementStyle.getPropertyValue('padding-top')) + parseInt(elementStyle.getPropertyValue('padding-bottom')),
+        elementPaddingHor = parseInt(elementStyle.getPropertyValue('padding-right')) + parseInt(elementStyle.getPropertyValue('padding-left')),
+        availableHeight = parentElementHeight - elementPaddingVer,
+        availableWidth = parentElementWidth - elementPaddingHor,
+        container = term.rowContainer,
+        subjectRow = term.rowContainer.firstElementChild,
+        contentBuffer = subjectRow.innerHTML,
+        characterHeight,
+        rows,
+        characterWidth,
+        cols,
+        geometry;
+
+    subjectRow.style.display = 'inline';
+    subjectRow.innerHTML = 'W'; // Common character for measuring width, although on monospace
+    characterWidth = subjectRow.getBoundingClientRect().width;
+    subjectRow.style.display = ''; // Revert style before calculating height, since they differ.
+    characterHeight = subjectRow.getBoundingClientRect().height;
+    subjectRow.innerHTML = contentBuffer;
+
+    rows = parseInt(availableHeight / characterHeight);
+    cols = parseInt(availableWidth / characterWidth);
+
+    term.resize(cols, rows);
+};
 require([
     'jquery',
     'base/js/utils',
-    'git-sync/static/xterm.js-2.9.1/dist/xterm.js'
+    'components/xterm.js/dist/xterm',
 ], function($, utils, Terminal) {
     function redirect_url() {
         var path = utils.get_body_data('path');
@@ -12,8 +49,7 @@ require([
         }
     }
 
-    var base_url = utils.get_body_data('baseUrl');
-    console.log(base_url);
+    var base_url = utils.get_body_data('base-url');
 
     var sync_url = base_url + 'git-sync/api?' + $.param({
         repo: utils.get_body_data('repo'),
@@ -32,20 +68,36 @@ require([
         var data = JSON.parse(ev.data);
         if (data.phase == 'Finished') {
             es.close();
-            // window.location.href = redirect_url();
+            $('#status-panel-title').css('width', '100%');
+            clearInterval(progressTimer);
+            window.location.href = redirect_url();
         } else if (data.phase == 'Syncing') {
             term.write(data.output);
         }
+    });
+
+    $('#status-panel-toggle').click(function() {
+        $('#pull-status-container').toggleClass('hidden');
+        fitTerm(term);
     });
     $('#header, #site').show();
 
     // Make sure we provide plenty of appearances of progress!
     setInterval(function() {
-        $('#sub-status').text(substatus_messages[Math.floor(Math.random() * substatus_messages.length)]);
+        $('#status-panel-title').text(substatus_messages[Math.floor(Math.random() * substatus_messages.length)]);
     }, 3000);
     setInterval(function() {
-        $('#sub-status').text($('#sub-status').text() + '.');
+        $('#status-panel-title').text($('#status-panel-title').text() + '.');
     }, 800);
+    var progressTimer = setInterval(function() {
+        var progress = $('#status-panel-title');
+        var val = parseFloat(progress.attr('aria-valuenow'));
+        // Illusion of progress!
+        var newVal = val + 0.05 * (100 - val);
+        progress.attr('aria-valuenow', newVal);
+        progress.css('width', newVal + '%');
+        console.log(newVal);
+    }, 900);
     var substatus_messages = [
         "Adding Hidden Agendas",
         "Adjusting Bell Curves",
