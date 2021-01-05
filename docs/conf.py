5d443c6065d52f943a8311290277a3b80c218796
YuviPanda <yuvipanda@gmail.com>
2021-01-05 17:57:23 +0530

    Remove unnecessary myst config

--- docs/conf.py
+++ docs/conf.py
@@ -26,8 +26,6 @@ extensions = [
     "sphinx.ext.intersphinx",
 ]
 
-myst_admonition_enable = True
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
