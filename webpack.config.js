b53a2c3cdf35862cb07fec61bddf70f1a3e7f6f7
GeorgianaElena <georgiana.dolocan@gmail.com>
2021-08-26 15:17:23 +0300

    Try minimizing bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,42 @@
 const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+var mode = process.env.NODE_ENV || 'development';
 
 module.exports = {
     context: __dirname + "/nbgitpuller/static/",
     entry: "./js/index.js",
     output: {
         path: __dirname + "/nbgitpuller/static/dist/",
-        filename: "bundle.js",
+        filename: "[name].js",
         publicPath: '/static/dist/'
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
             },
         ]
     },
-    devtool: 'source-map',
+    devtool: (mode === 'development') ? 'inline-source-map' : false,
+    mode: mode,
+    optimization: {
+        minimize: true,
+        minimizer: [
+            `...`,
+            new CssMinimizerPlugin(),
+        ],
+    },
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery',
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css",
         }),
     ]
 }
