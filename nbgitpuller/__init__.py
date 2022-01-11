f6185606818fceb2b9b5f76e7b34a4bb5473051a
Sean Morris <sean.smorris@berkeley.edu>
2022-01-10 18:40:01 -0800

    Removed nested_asyncio from init.py
    The nested_asyncio import and call are moved to the nbgitpuller
    downloader plugins

--- nbgitpuller/__init__.py
+++ nbgitpuller/__init__.py
@@ -4,12 +4,6 @@ from .pull import GitPuller # noqa
 from notebook.utils import url_path_join
 from tornado.web import StaticFileHandler
 import os
-import nest_asyncio
-
-# this allows us to nest usage of the event_loop from asyncio
-# being used by tornado in jupyter distro
-# Ref: https://medium.com/@vyshali.enukonda/how-to-get-around-runtimeerror-this-event-loop-is-already-running-3f26f67e762e
-nest_asyncio.apply()
 
 
 def _jupyter_server_extension_paths():
