jupyterhub/nbgitpuller

commit 1b3083f8b0131c0b95d2c2e4db91f76ed959c838

When resetting deleted files, take from local copy
Fixes issues #254 and #234.
When a deleted file is reset, this checks it out from the local copy
of the branch, rather than from the remote copy. The file might not
exist in the remote branch anymore, causing #234 when running
`git checkout`. If the file was conflicted before, we will run into
issue #254.

If the remote file has changed, it will be merged regularly afterwards.

nbgitpuller/pull.py
@@ -172,7 +172,7 @@ class GitPuller(Configurable):
 
         for filename in deleted_files:
             if filename:  # Filter out empty lines
-                yield from execute_cmd(['git', 'checkout', 'origin/{}'.format(self.branch_name), '--', filename], cwd=self.repo_dir)
+                yield from execute_cmd(['git', 'checkout', '--', filename], cwd=self.repo_dir)
 
     def repo_is_dirty(self):
         """

commit 427d92c989cc74c2696b41074e6d7a3268b2f557

Fix bug where files could not be reset properly
Commit 1b3083f introduced a bug, where resetting a file restored an
older version instead of a new version from the repository.  This
reverts the commit.  Instead of taking the reset file from the local
branch, it takes it from the remote branch again, but first checks if it
exists.

nbgitpuller/pull.py
@@ -170,9 +170,13 @@ class GitPuller(Configurable):
             'git', 'ls-files', '--deleted', '-z'
         ], cwd=self.repo_dir).decode().strip().split('\0')
 
+        paths = self.find_upstream_changed('D')
+        upstream_deleted = [fn[len(self.repo_dir) + 1:] for fn in paths]
+
         for filename in deleted_files:
-            if filename:  # Filter out empty lines
-                yield from execute_cmd(['git', 'checkout', '--', filename], cwd=self.repo_dir)
+            # Filter out empty lines, and files that were deleted in the remote
+            if filename and filename not in upstream_deleted:
+                yield from execute_cmd(['git', 'checkout', 'origin/{}'.format(self.branch_name), '--', filename], cwd=self.repo_dir)
 
     def repo_is_dirty(self):
         """