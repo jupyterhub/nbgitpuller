04930d2dcefe0866463ab0b5e29be2d718e078b6
Erik Sundell <erik@sundellopensource.se>
2024-03-29 22:06:42 +0100

    Bump to 1.2.2.dev

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ cmdclass = wrap_installers(
 
 setup(
     name='nbgitpuller',
-    version="1.2.1",
+    version="1.2.2.dev",
     url='https://github.com/jupyterhub/nbgitpuller',
     license='3-clause BSD',
     author='Peter Veerman, YuviPanda',
