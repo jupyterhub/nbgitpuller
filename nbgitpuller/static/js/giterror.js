93b91def511ac6afab43d5f558aa2468574e114d
jnywong <jnywong.pro@gmail.com>
2025-12-10 12:28:53 +0000

    Update helper text

--- nbgitpuller/static/js/giterror.js
+++ nbgitpuller/static/js/giterror.js
@@ -2,5 +2,10 @@ export function GitError(message) {
   const s = message.toLowerCase();
 
   if (s.includes("merge"))
-    return `<p class="lead">Unresolvable conflicts detected while syncing</p><p><strong>Proceed without syncing</strong> to continue with the current state of your repository with no updates.</p><p><strong>Backup and resync</strong> to backup the current state of your repository (renamed with a timestamp) and pull in a fresh copy of the source content into a new folder (named with no timestamp).</p>`;
+    return `<p class="lead">Unresolvable conflicts detected while syncing</p><p><strong>Proceed without syncing</strong> to continue with the current state of your repository without updates.</p><p><strong>(Recommended) Backup and resync</strong> to backup the current state of your repository and sync updates into a new separate folder:<dl class="dl-horizontal">
+    <dt><code>backup folder name</code></dt>
+    <dd>Backup folder containing the current state of your repository</dd>
+    <dt><code>new folder name</code></dt>
+    <dd>New folder containing updated content. This will not merge content from your backup due to the unresolvable conflicts. You may want to manually copy across your work into the new folder.</dd>
+    </dl></p>`;
 }
