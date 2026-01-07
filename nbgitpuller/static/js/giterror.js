21063dacf606dc7def4e6ce0d5fd17c4ada06f2d
jnywong <jnywong.pro@gmail.com>
2026-01-07 14:59:17 +0000

    Add link to docs

--- nbgitpuller/static/js/giterror.js
+++ nbgitpuller/static/js/giterror.js
@@ -10,6 +10,6 @@ export function GitError(gitsync, message) {
     return `<p class="lead">Unresolvable conflicts detected while syncing</p><p><strong>Proceed without syncing</strong> to continue with the current state of your repository without updates.</p><p><strong>(Recommended) Backup and resync</strong> to backup the current state of your repository and sync updates into a new separate folder:<ul>
     <li><code>${path}_backup_YYYYMMDDHHMMSS</code> Timestamped backup folder containing the current state of your repository</li>
     <li><code>${path}</code> New folder containing updated content. This will not merge content from your backup due to the unresolvable conflicts. You may want to manually copy backed up changes into the new folder.</li>
-    </ul></p>
+    </ul><a href="https://nbgitpuller.readthedocs.io/en/latest/topic/automatic-merging.html">See more about automatic merging behavior.</a></p>
     <a href=${url} class="btn btn-warning role="button" style="margin-top: 6px;"aria-label="Backup and resync, then go to Jupyter server.">Backup and resync</a>`;
 }
