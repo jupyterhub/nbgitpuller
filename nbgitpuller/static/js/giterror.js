68f232a0d25df732e481b5a69fc285b541c1e9ae
jnywong <jnywong.pro@gmail.com>
2026-01-13 10:30:20 +0000

    Remove "recommended" text

--- nbgitpuller/static/js/giterror.js
+++ nbgitpuller/static/js/giterror.js
@@ -7,9 +7,9 @@ export function GitError(gitsync, message) {
   url.searchParams.append("backup", "true");
 
   if (s.includes("merge"))
-    return `<p class="lead">Unresolvable conflicts detected while syncing</p><p><strong>(Recommended) Backup and resync</strong> to backup the current state of your repository and sync updates into a new separate folder:<ul>
+    return `<p class="lead">Unresolvable conflicts detected while syncing</p><p><strong>Backup and resync</strong> to backup the current state of your repository and sync updates into a new separate folder:<ul>
     <li><code>${path}_backup_YYYYMMDDHHMMSS</code> Timestamped backup folder containing the current state of your repository</li>
-    <li><code>${path}</code> New folder containing updated content. This will not merge content from your backup due to the unresolvable conflicts. You may want to manually copy backed up changes into the new folder.</li>
-    </ul></p><p><strong>Proceed without syncing</strong> to continue with the current state of your repository without updates.</p><p><a href="https://nbgitpuller.readthedocs.io/en/latest/topic/automatic-merging.html">See more about automatic merging behavior.</a></p>
-    <a href=${url} class="btn btn-primary role="button" style="margin-top: 6px;"aria-label="Backup and resync, then go to Jupyter server.">Backup and resync</a>`;
+    <li><code>${path}</code> New folder containing updated content. <em>This new folder will not merge content from your backup due to the unresolvable conflicts.</em> You may want to manually copy backed up changes into the new folder.</li>
+    </ul></p><p><strong>Proceed without syncing</strong> to continue with the current state of your repository without any new updates.</p><p><a href="https://nbgitpuller.readthedocs.io/en/latest/topic/automatic-merging.html">See more about automatic merging behavior.</a></p>
+    <a href=${url} class="btn btn-primary role="button" style="margin-right: 5px" aria-label="Backup and resync, then go to Jupyter server.">Backup and resync</a>`;
 }
