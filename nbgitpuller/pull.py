3c72df96f0f7c8ccffc77b492c8ae87ca8b120ae
jnywong <jnywong.pro@gmail.com>
2026-01-22 15:59:55 +0000

    Use local vars

--- nbgitpuller/pull.py
+++ nbgitpuller/pull.py
@@ -74,19 +74,19 @@ class GitPuller(Configurable):
 
         self.git_url = git_url
         self.branch_name = kwargs.pop("branch")
-        self.backup = kwargs.pop("backup", False)
         self.repo_dir = repo_dir
+        backup = kwargs.pop("backup", False)
 
         if self.branch_name is None:
             self.branch_name = self.resolve_default_branch()
         elif not self.branch_exists(self.branch_name):
             raise ValueError(f"Branch: {self.branch_name} -- not found in repo: {self.git_url}")
 
-        if self.backup and os.path.exists(self.repo_dir):
+        if backup and os.path.exists(self.repo_dir):
             timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-            self.backup_dir = f"{self.repo_dir}_backup_{timestamp}"
-            backup = os.rename(self.repo_dir, self.backup_dir)
-            logging.info('Backed up folder {}'.format(self.backup_dir)) 
+            backup_dir = f"{self.repo_dir}_backup_{timestamp}"
+            os.rename(self.repo_dir, backup_dir)
+            logging.info('Backed up folder {}'.format(backup_dir)) 
 
         newargs = {k: v for k, v in kwargs.items() if v is not None}
         super(GitPuller, self).__init__(**newargs)
