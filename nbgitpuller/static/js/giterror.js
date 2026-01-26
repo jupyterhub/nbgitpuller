559893527061a4280afa381a97c2d20d09950355
jnywong <jnywong.pro@gmail.com>
2026-01-26 10:06:30 +0000

    Separate backup and resync button

--- nbgitpuller/static/js/giterror.js
+++ nbgitpuller/static/js/giterror.js
@@ -20,12 +20,16 @@ export function GitError(gitsync, data) {
   ;
 };
 
+function ButtonBackupAndResync(url) {
+  return `<a href=${url} class="btn btn-primary role="button" style="margin-right: 5px" aria-label="Backup and resync, then go to Jupyter server.">Backup and resync</a>`
+};
+
 function MergeConflictHelp (data, path, url) {
   return `<p class="lead">${data.error.message}</p><p><strong>Backup and resync</strong> to backup the current state of your repository and sync updates into a new separate folder:<ul>
   <li><code>${path}_backup_YYYYMMDDHHMMSS</code> Timestamped backup folder containing the current state of your repository</li>
   <li><code>${path}</code> New folder containing updated content. <em>This new folder will not merge content from your backup due to the unresolvable conflicts.</em> You may want to manually copy backed up changes into the new folder.</li>
   </ul></p><p><strong>Proceed without syncing</strong> to continue with the current state of your repository without any new updates.</p><p><a href="https://nbgitpuller.readthedocs.io/en/latest/topic/automatic-merging.html">See more about automatic merging behavior.</a></p>
-  <a href=${url} class="btn btn-primary role="button" style="margin-right: 5px" aria-label="Backup and resync, then go to Jupyter server.">Backup and resync</a>`;
+  ${ButtonBackupAndResync(url)}`;
 };
 
 
